3f32a34d3dbd6e5ebae03d420f3ed8b609f152c1
Sean Ardley <sean.ardley@gmail.com>
2017-03-03 14:31:07 -0800

    added line in devbranch

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Added this line in devbranch
+
 setup.git
 =========
 Clone and run this on a new EC2 instance running Ubuntu 12.04.2 LTS to
--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+# added this line in devbranch
+
 #!/bin/bash
 # Simple setup.sh for configuring Ubuntu 12.04 LTS EC2 instance
 # for headless setup. 
